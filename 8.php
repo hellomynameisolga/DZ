33f720e88df9d392f717f7d65e54abe965963a0e
Molchanova Olga <hellomynameisolga@gmail.com>
2016-11-01 14:10:41 +0500

    Vottakvot

--- 8.php
+++ 8.php
@@ -1,13 +1,15 @@
-<?php
+﻿<?php
 $str="Нет будущего другого, кроме того, что мы творим сами.";
 echo "$str </br>";
 $arr=explode(" ", $str);
 print_r($arr);
-$i=count($arr);
+$i=count($arr)-1;
 $j=0;
 do {$d[$j]=$arr[$i];
  $i--;
  $j++;
 } while ($i>=0);
-echo $d[$j];
+$stroke=implode("!", $d);
+echo ("</br>");
+echo ("$stroke");
 ?>
