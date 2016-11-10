21037b709f0b32575f6363b5013ea9f386a06721
Molchanova Olga <hellomynameisolga@gmail.com>
2016-11-10 16:03:06 +0500

    peredelka

--- DZ_2/3.php
+++ DZ_2/3.php
@@ -1,61 +1,57 @@
-<?php
+﻿<?php
 $b="/";
-$a=5;
-$d=2;
+$a=8;
+$h=1;
 $c=3;
-$e=9;
-function event( ){
-$p=func_num_args();
-$arrayargs=func_get_args();
-/*print_r($arrayargs);*/
+function event( )
+{
+    $p=func_num_args();
+    $arrayargs=func_get_args();
+   # print_r($arrayargs);
+   #echo "<br>";
+    switch ($arrayargs[0])
+    {
+        case '-':
+            for ($i=1; $i < count($arrayargs); $i++) 
+            {
+                for ($j=2; $j < count($arrayargs); $j++) 
+                {
+               $d=$arrayargs[$i];
+               $arrayargs[$i]=$arrayargs[$i]-$arrayargs[$j];
 
-	switch ($arrayargs[0]) {
-	case '+':
-		$summa=0;
-	for ($i=1; $i <= count($arrayargs); $i++) { 
-				$summa=$summa+$arrayargs[$i];
-			}
-			echo("$summa");
-		break;
-	case '-':
-		$raznost=$arrayargs[1];
-	for ($i=1; $i <= count($arrayargs); $i++) { 
-				$raznost=$raznost-$arrayargs[$i+1];
-			}
-			echo("$raznost");
-		break;
-	case '/':
-	$division=$arrayargs[1];
-	for ($i=1; $i <= count($arrayargs); $i++) { 
-$division=$division / $arrayargs[$i+1];
-echo "$division";
-			}
+                }
 
-		break;
-	case '*':
-	$proizvedenie=1;
-	for ($i=1; $i <= count($arrayargs); $i++) { 
-				$proizvedenie=$proizvedenie*$arrayargs[$i];
-			}
-			echo("$proizvedenie");
-		break;
-	
-	default:
-		# code...
-		break;
-
+            }
+        break;
+        case '/':
+            for ($i=1; $i < count($arrayargs); $i++) 
+            {
+                for ($j=2; $j < count($arrayargs) ; $j++) 
+                {
+                $d=$arrayargs[$i];
+                $arrayargs[$i]=$arrayargs[$i]/$arrayargs[$j];
+                }
+            }
+        break;
+        case '*':
+            $d=1;
+            for ($i=1; $i < count($arrayargs); $i++) 
+            {
+                $d=$d*$arrayargs[$i];
+            }
+       
+        break;
+        case '+':
+       		 $d=0;
+       		 for ($i=1; $i < count($arrayargs); $i++)
+       		{
+       		 $d=$d+$arrayargs[$i];
+           	}
+        
+        break;
+    }
+    return $d;
 }
-}
-event($b, $a, $d, $c, $e);
-/*
-1.	Функция должна принимать переменное число аргументов.
-2.	Первым аргументом обязательно должна быть строка, обозначающая арифметическое действие, которое необходимо выполнить со всеми передаваемыми аргументами.
-3.	Остальные аргументы это целые и/или вещественные числа.
+echo event($b, $a, $h, $c);
 
-Пример вызова: calcEverything(‘+’, 1, 2, 3, 5.2);
-Результат: 1 + 2 + 3 + 5.2 = 11.2
-
-
-
-*/
 ?>
